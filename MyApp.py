e85c38cf7a3a5fa5fac22e4e08fc721a07b52f5f
Sabri <sabri.rakrouki1@gmail.com>
2025-06-26 19:50:02 +0100

    poject

---
+++ MyApp.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+from joblib import load
+
+st.title('Hello World')
+region=st.number_input('REGION')
+montant=st.number_input('MONTANT')
+frequence=st.number_input('FREQUENCE_RECH')
+revenue=st.number_input('REVENUE')
+arpu=st.number_input('ARPU_SEGMENT')
+frequen=st.number_input('FREQUENCE')
+data=st.number_input('DATA_VOLUME')
+net=st.number_input('ON_NET')
+orange=st.number_input('ORANGE')
+tigo=st.number_input('TIGO')
+zone1=st.number_input('ZONE1')
+zone2=st.number_input('ZONE2')
+mrg=st.number_input('MRG')
+regularity=st.number_input('REGULARITY')
+freq=st.number_input('FREQ_TOP_PACK')
+
+
+
+model = load('model.joblib')
+
+
+
+
+if st.button("Predict"):
+    prediction = model.predict([[region, montant,frequence,revenue,arpu,frequen,data,net,orange,tigo,zone1,zone2,mrg,regularity,freq ]])
+    st.write(f"Prediction: {prediction[0]}")
